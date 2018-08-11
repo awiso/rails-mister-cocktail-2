1ceb99283a4d2541399fb41ec45bbd0e21cade54
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-11 12:03:04 +0200

    all actions working

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   
   has_many :cocktails, through: :doses
-  has_many :doses
+  has_many :doses, dependent: :destroy
 end
